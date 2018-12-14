00b6ead0eb502e3dcbf43af431fb5d2716b73728
vahidmajedi <vahid.majedi@gmail.com>
2018-12-14 10:03:18 +0330

    ver1.0.2-Beep Sound Added

--- MiniProjects/Start_Form/MainForm.cs
+++ MiniProjects/Start_Form/MainForm.cs
@@ -16,6 +16,7 @@ namespace MiniGames
         public static string Operator = "+";
         int result, score = 0, test_run = 0;
         float CurrentTime = 0;
+        bool FinishFlag = false;
 
         public frmMain()
         {
@@ -25,6 +26,7 @@ namespace MiniGames
         private void frmMain_Load(object sender, EventArgs e)
         {
             pbTime.Value = pbTime.Maximum;
+            lblOperator.Text = Operator;
         }
 
         private void btnNo_Click(object sender, EventArgs e)
@@ -45,14 +47,13 @@ namespace MiniGames
 
         private void btnStart_Click(object sender, EventArgs e)
         {
+            lblOperator.Text = Operator;
             Random r = new Random();
             int operand1 = r.Next(MinOperand, MaxOperand);
             int operand2 = r.Next(MinOperand, MaxOperand);
             lblOperand1.Text = operand1.ToString();
             lblOperand2.Text = operand2.ToString();
-            lblOperator.Text = Operator;
-            CurrentTime = 0;
-            timer.Enabled = true;
+
             switch (Operator)
             {
                 case "+":
@@ -71,22 +72,30 @@ namespace MiniGames
                     result = operand1 % operand2;
                     break;
             }
+
+            CurrentTime = 0;
+            timer.Start();
         }
 
         private void timer_Tick(object sender, EventArgs e)
         {
+            float err = 0.05F;
             CurrentTime += (float)timer.Interval / 1000;
             if (CurrentTime > MaxTime)
             {
-                timer.Enabled = false;
+                timer.Stop();
                 pbTime.Value = pbTime.Minimum;
                 lblTime.Text = "0";
+                Console.Beep(2000, 1000);
             }
-
             else
             {
-                pbTime.Value = pbTime.Maximum - (int)(CurrentTime * pbTime.Maximum) / MaxTime;
+                pbTime.Value = pbTime.Maximum - (int)((CurrentTime * pbTime.Maximum) / MaxTime);
                 lblTime.Text = (MaxTime - Math.Floor(CurrentTime)).ToString();
+                if (((MaxTime - CurrentTime) < (3 + err) && (MaxTime - CurrentTime) > (3 - err)) ||
+                    ((MaxTime - CurrentTime) < (2 + err) && (MaxTime - CurrentTime) > (2 - err)) ||
+                    ((MaxTime - CurrentTime) < (1 + err) && (MaxTime - CurrentTime) > (1 - err)))
+                    Console.Beep(2000, 400);
             }
         }
 
@@ -104,6 +113,8 @@ namespace MiniGames
                 score++;
             tstlbl.Text = score + "/" + test_run;
             txtResult.Text = null;
+            CurrentTime = 0;
+            timer.Stop();
         }
 
         private void settingToolStripMenuItem_Click(object sender, EventArgs e)
